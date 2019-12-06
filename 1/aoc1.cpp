03212370aca909a9d06cf260faacc84fbe6a530c
yoshi <hess.jonathan@gmail.com>
2019-12-06 23:09:36 +0000

    implemented reading lines

--- 1/aoc1.cpp
+++ 1/aoc1.cpp
@@ -1,5 +1,6 @@
 #include <iostream>
 #include <cstdint>
+#include <string>
 
 #include "aoc1.h"
 
@@ -7,7 +8,12 @@ int main (int argc, char** argv)
 {
   std::int64_t fuel = 0;
 
-
+  std::string module_mass;
+  while (getline (std::cin, module_mass))
+  {
+    fuel += AdventOfCode::fuel_for_module (std::stoi (module_mass));
+  }
+  std::cout << "fuel: " << fuel << std::endl;
 
   return 0;
 }
