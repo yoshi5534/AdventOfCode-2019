fbd01a00432af0361ff5c1731c8b2d2b28df08e4
yoshi <hess.jonathan@gmail.com>
2019-12-06 23:28:50 +0000

    add solution for star 2

--- 1/aoc1.h
+++ 1/aoc1.h
@@ -2,8 +2,17 @@
 
 namespace AdventOfCode
 {
-int fuel_for_module (int mass)
+int fuel_calculation (int mass)
 {
   return static_cast <int> (std::floor (static_cast <double> (mass) / 3.0) - 2.0);
+}
+
+int fuel_for_module (int mass)
+{
+  int fuel = 0;
+  while ((mass = fuel_calculation (mass)) > 0)
+    fuel += mass;
+
+  return fuel;
 }
 };
