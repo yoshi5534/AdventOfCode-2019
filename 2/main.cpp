ecf54bf83bbf2e267fbff01844fb58cf6f030a4d
yoshi <hess.jonathan@gmail.com>
2019-12-07 18:54:32 +0000

    find correct noun and verb

--- 2/main.cpp
+++ 2/main.cpp
@@ -8,6 +8,14 @@
 int main (int argc, char** argv)
 {
   using namespace AdventOfCode;
+  int noun = 12;
+  int verb = 2;
+
+  if (argc == 3)
+  {
+    noun = std::stoi(argv[1]);
+    verb = std::stoi(argv[2]);
+  }
 
   std::string program_text;
   getline (std::cin, program_text);
@@ -21,16 +29,11 @@ int main (int argc, char** argv)
     program_stream.seekg (1, std::ios_base::cur);
   }
 
-  program [1] = 12;
-  program [2] = 2;
-  
+  program [1] = noun;
+  program [2] = verb;
+
   Computer::calculate (program);
-  std::cout << "Result: \n";
-  for (auto const& opcode: program)
-  {
-    std::cout << opcode << ",";
-  }
-  std::cout << std::endl;
+  std::cout << program [0];
 
   return 0;
 }
