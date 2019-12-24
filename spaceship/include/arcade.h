7de26ff9f060df871d17f984f4bcbfc5b39a5fd7
yoshi <hess.jonathan@gmail.com>
2019-12-24 21:19:23 +0000

    tighten the interface

--- spaceship/include/arcade.h
+++ spaceship/include/arcade.h
@@ -9,37 +9,6 @@
 
 namespace AdventOfCode {
 
-struct ScreenPosition {
-  int x;
-  int y;
-
-  ScreenPosition operator+(ScreenPosition const &other) const {
-    return {x + other.x, y + other.y};
-  }
-  bool operator==(ScreenPosition const &other) const {
-    return x == other.x && y == other.y;
-  }
-
-  constexpr bool operator<(ScreenPosition const &other) const {
-    if (y < other.y)
-      return true;
-    if (y == other.y)
-      return x < other.x;
-
-    return false;
-  }
-};
-
-struct Empty {};
-struct Wall {};
-struct Block {};
-struct Paddle {};
-struct Ball {};
-
-using TileId = std::variant<Empty, Wall, Block, Paddle, Ball>;
-using Tile = std::pair<ScreenPosition, TileId>;
-using Screen = std::map<ScreenPosition, TileId>;
-
 class ArcadeCabinet {
 public:
   ArcadeCabinet(Program const &program);
@@ -48,27 +17,11 @@ public:
   int getScore () const;
 
 private:
-  int getNextOutput();
-  std::optional<Tile> getNextTile();
-  
-  void moveJoystick();
   void updateScore();
-
-  auto getScreenDimension() const;
-
-  auto predictDirection(ScreenPosition newBallPosition) const;
-
-  void drawTile(int x);
-
-  void printScreen() const;
 
 private:
   Computer computer_;
-  Screen screen_;
   int score_;
-  ScreenPosition paddlePosition_;
-  ScreenPosition ballPosition_;
-  ScreenPosition direction_;
 };
 
 } // namespace AdventOfCode
--- spaceship/src/arcade.cpp
+++ spaceship/src/arcade.cpp
@@ -1,6 +1,5 @@
 #include <arcade.h>
 
-
 #include <computer.h>
 
 #include <algorithm>
@@ -14,222 +13,265 @@
 using namespace AdventOfCode;
 
 namespace {
+struct ScreenPosition {
+  int x;
+  int y;
 
-  TileId getTileId(int id) {
-    if (id == 0)
-      return Empty{};
-    if (id == 1)
-      return Wall{};
-    if (id == 2)
-      return Block{};
-    if (id == 3)
-      return Paddle{};
-    if (id == 4)
-      return Ball{};
-
-    throw std::runtime_error("no known tile id");
+  ScreenPosition operator+(ScreenPosition const &other) const {
+    return {x + other.x, y + other.y};
+  }
+  bool operator==(ScreenPosition const &other) const {
+    return x == other.x && y == other.y;
   }
 
-  int getId(TileId tile) {
-    if (std::holds_alternative<Empty>(tile))
-      return 0;
-    if (std::holds_alternative<Wall>(tile))
-      return 1;
-    if (std::holds_alternative<Block>(tile))
-      return 2;
-    if (std::holds_alternative<Paddle>(tile))
-      return 3;
-    if (std::holds_alternative<Ball>(tile))
-      return 4;
+  constexpr bool operator<(ScreenPosition const &other) const {
+    if (y < other.y)
+      return true;
+    if (y == other.y)
+      return x < other.x;
 
-    throw std::runtime_error("no known tile id");
+    return false;
+  }
+};
+
+struct Empty {};
+struct Wall {};
+struct Block {};
+struct Paddle {};
+struct Ball {};
+
+using TileId = std::variant<Empty, Wall, Block, Paddle, Ball>;
+using Tile = std::pair<ScreenPosition, TileId>;
+using Screen = std::map<ScreenPosition, TileId>;
+
+struct GameState{
+  GameState () : paddlePosition {0,0}, ballPosition {0,0}, direction {0,-1} {}
+  
+  ScreenPosition paddlePosition;
+  ScreenPosition ballPosition;
+  ScreenPosition direction;
+};
+
+TileId getTileId(int id) {
+  if (id == 0)
+    return Empty{};
+  if (id == 1)
+    return Wall{};
+  if (id == 2)
+    return Block{};
+  if (id == 3)
+    return Paddle{};
+  if (id == 4)
+    return Ball{};
+
+  throw std::runtime_error("no known tile id");
+}
+
+int getId(TileId tile) {
+  if (std::holds_alternative<Empty>(tile))
+    return 0;
+  if (std::holds_alternative<Wall>(tile))
+    return 1;
+  if (std::holds_alternative<Block>(tile))
+    return 2;
+  if (std::holds_alternative<Paddle>(tile))
+    return 3;
+  if (std::holds_alternative<Ball>(tile))
+    return 4;
+
+  throw std::runtime_error("no known tile id");
+}
+
+int getNextOutput(Computer& computer) {
+  Intcode code;
+  do {
+    code = computer.runInstruction();
+  } while (code != Intcode::Output && code != Intcode::Halt);
+  if (code == Intcode::Halt)
+    return -99;
+
+  return computer.readOutput();
+}
+
+std::optional<Tile> getNextTile(Computer& computer) {
+  auto x = getNextOutput(computer);
+  if (x < 0)
+    return std::nullopt;
+
+  auto y = getNextOutput(computer);
+  auto tileId = getTileId(getNextOutput(computer));
+
+  return Tile{{x, y}, tileId};
+}
+
+void moveJoystick(Computer& computer, GameState& state) {
+  if (state.paddlePosition == state.ballPosition)
+    computer.writeInput({state.direction.x});
+  if (state.paddlePosition.x < state.ballPosition.x + state.direction.x)
+    computer.writeInput({1});
+  else if (state.paddlePosition.x > state.ballPosition.x + state.direction.x)
+    computer.writeInput({-1});
+  else
+    computer.writeInput({state.direction.x});
+}
+
+auto getScreenDimension(Screen& screen) {
+  std::vector<int> xValues;
+  std::vector<int> yValues;
+  std::for_each(std::begin(screen), std::end(screen), [&](auto tile) {
+    auto pos = tile.first;
+    xValues.push_back(pos.x);
+    yValues.push_back(pos.y);
+  });
+
+  auto &&[minX, maxX] = std::minmax_element(begin(xValues), end(xValues));
+  auto &&[minY, maxY] = std::minmax_element(begin(yValues), end(yValues));
+
+  int width = *maxX - *minX + 1;
+  int height = *maxY - *minY + 1;
+
+  return std::make_pair(width, height);
+}
+
+void printScreen(Screen& screen) {
+  auto [width, height] = getScreenDimension(screen);
+  for (int y = 0; y < height; ++y) {
+    for (int x = 0; x < width; ++x) {
+      auto tile = screen.find({x, y});
+      if (tile != std::end(screen)) {
+        if (std::holds_alternative<Empty>(tile->second))
+          std::cout << ' ';
+        if (std::holds_alternative<Wall>(tile->second))
+          std::cout << 'W';
+        if (std::holds_alternative<Block>(tile->second))
+          std::cout << '#';
+        if (std::holds_alternative<Paddle>(tile->second))
+          std::cout << '-';
+        if (std::holds_alternative<Ball>(tile->second))
+          std::cout << '@';
+      } else
+        std::cout << ' ';
+    }
+    std::cout << '\n';
   }
 }
-  ArcadeCabinet::ArcadeCabinet(Program const &program)
-      : computer_{program}, screen_{}, score_{0}, paddlePosition_{0, 0},
-        ballPosition_{0, 0}, direction_{1, -1} {}
 
-  void ArcadeCabinet::play() {
-    computer_.writeInput({0});
-    while (true) {
-      Intcode code;
-      do {
-        code = computer_.runInstruction();
-      } while (code != Intcode::Output && code != Intcode::Input &&
-               code != Intcode::Halt);
-      if (code == Intcode::Halt)
-        break;
-      if (code == Intcode::Input)
-        moveJoystick();
-      if (code == Intcode::Output) {
-        auto output = computer_.readOutput();
-        if (output == -1)
-          updateScore();
-        else
-          drawTile(output);
-      }
+void predictDirection(Screen& screen, GameState& state, ScreenPosition newBallPosition) {
+  printScreen(screen);
+  auto [width, height] = getScreenDimension(screen);
+  ScreenPosition direction{state.direction};
+
+  if (newBallPosition.x > state.ballPosition.x)
+    direction.x = 1;
+  else if (newBallPosition.x < state.ballPosition.x)
+    direction.x = -1;
+
+  if (newBallPosition.y > state.ballPosition.y)
+    direction.y = 1;
+
+  if (newBallPosition.x == 1)
+    direction.x = 1;
+  if (newBallPosition.x == width - 2)
+    direction.x = -1;
+
+  if (newBallPosition.y == 1)
+    direction.y = 1;
+  if (newBallPosition.y == height - 3)
+    direction.y = -1;
+
+  auto hittedTile = screen.find(newBallPosition + direction);
+  auto neighbourBall =
+      screen.find({newBallPosition.x + direction.x, newBallPosition.y});
+  auto aboveBall =
+      screen.find({newBallPosition.x, newBallPosition.y + direction.y});
+  if (std::holds_alternative<Block>(hittedTile->second)) {
+    if (std::holds_alternative<Empty>(neighbourBall->second))
+      direction.y = direction.y * -1;
+    else if (std::holds_alternative<Block>(aboveBall->second))
+      direction.y = direction.y * -1;
+
+    if (std::holds_alternative<Empty>(aboveBall->second))
+      direction.x = state.direction.x * -1;
+    else if (std::holds_alternative<Block>(neighbourBall->second))
+      direction.x = state.direction.x * -1;
+  } else if (std::holds_alternative<Block>(neighbourBall->second)) {
+    auto aboveBall =
+        screen.find({newBallPosition.x, newBallPosition.y + direction.y});
+    auto nextHitBall = screen.find(
+        {newBallPosition.x - direction.x, newBallPosition.y + direction.y});
+    // #
+    //#@
+    if (std::holds_alternative<Block>(aboveBall->second))
+      direction.x = state.direction.x * -1;
+    //  #
+    //#@
+    else if (std::holds_alternative<Block>(
+                 nextHitBall->second)) { // direction is kept except
+      // #
+      // #@
+      //   #
+      auto oppositeHit = screen.find(
+          {newBallPosition.x + direction.x, newBallPosition.y - direction.y});
+      if (std::holds_alternative<Block>(oppositeHit->second))
+        direction.x = state.direction.x * -1;
     }
+    //#
+    //#@
+    else
+      direction.x = state.direction.x * -1;
   }
 
-  int ArcadeCabinet::getScore () const {return score_; }
+  state.direction = direction;
+}
 
-  int ArcadeCabinet::getNextOutput() {
+void drawTile(Computer& computer, Screen& screen, GameState& state, int x) {
+  auto y = getNextOutput(computer);
+  auto tileId = getTileId(getNextOutput(computer));
+  screen[{x, y}] = tileId;
+  if (std::holds_alternative<Paddle>(tileId))
+    state.paddlePosition = {x, y};
+  if (std::holds_alternative<Ball>(tileId)) {
+    predictDirection(screen, state, {x, y});
+    state.ballPosition = {x, y};
+  }
+}
+
+} // namespace
+ArcadeCabinet::ArcadeCabinet(Program const &program)
+    : computer_{program}, score_{0} {}
+
+void ArcadeCabinet::play() {
+
+  Screen screen {};
+  GameState state{};
+  computer_.writeInput({0});
+  while (true) {
     Intcode code;
     do {
       code = computer_.runInstruction();
-    } while (code != Intcode::Output && code != Intcode::Halt);
+    } while (code != Intcode::Output && code != Intcode::Input &&
+             code != Intcode::Halt);
     if (code == Intcode::Halt)
-      return -99;
-
-    return computer_.readOutput();
-  }
-
-  std::optional<Tile> ArcadeCabinet::getNextTile() {
-    auto x = getNextOutput();
-    if (x < 0)
-      return std::nullopt;
-
-    auto y = getNextOutput();
-    auto tileId = getTileId(getNextOutput());
-
-    return Tile{{x, y}, tileId};
-  }
-  
-  void ArcadeCabinet::moveJoystick() {
-    if (paddlePosition_ == ballPosition_)
-      computer_.writeInput({direction_.x});
-    if (paddlePosition_.x < ballPosition_.x + direction_.x)
-      computer_.writeInput({1});
-    else if (paddlePosition_.x > ballPosition_.x + direction_.x)
-      computer_.writeInput({-1});
-    else
-      computer_.writeInput({direction_.x});
-  }
-
-  void ArcadeCabinet::updateScore() {
-    auto y = getNextOutput();
-    if (y != 0)
-      throw std::runtime_error("should be zero for segment");
-
-    score_ = getNextOutput();
-  }
-
-  auto ArcadeCabinet::getScreenDimension() const {
-    std::vector<int> xValues;
-    std::vector<int> yValues;
-    std::for_each(std::begin(screen_), std::end(screen_), [&](auto tile) {
-      auto pos = tile.first;
-      xValues.push_back(pos.x);
-      yValues.push_back(pos.y);
-    });
-
-    auto &&[minX, maxX] = std::minmax_element(begin(xValues), end(xValues));
-    auto &&[minY, maxY] = std::minmax_element(begin(yValues), end(yValues));
-
-    int width = *maxX - *minX + 1;
-    int height = *maxY - *minY + 1;
-
-    return std::make_pair(width, height);
-  }
-
-  auto ArcadeCabinet::predictDirection(ScreenPosition newBallPosition) const {
-    printScreen();
-    [[maybe_unused]] auto [width, height] = getScreenDimension();
-    ScreenPosition direction{direction_};
-
-    if(newBallPosition. x > ballPosition_.x)
-      direction.x = 1;
-    else if(newBallPosition. x < ballPosition_.x)
-      direction.x = -1;
-
-    if(newBallPosition. y > ballPosition_.y)
-      direction.y = 1;
-
-    if (newBallPosition.x == 1)
-      direction.x = 1;
-    if (newBallPosition.x == width - 2)
-      direction.x = -1;
-
-    if (newBallPosition.y == 1)
-      direction.y = 1;
-    if (newBallPosition.y == height - 3)
-      direction.y = -1;
-
-    auto hittedTile = screen_.find(newBallPosition + direction);
-    auto neighbourBall = screen_.find({newBallPosition.x + direction.x, newBallPosition.y});
-    auto aboveBall = screen_.find({newBallPosition.x, newBallPosition.y + direction.y});
-    if (std::holds_alternative<Block>(hittedTile->second))
-    {
-      if (std::holds_alternative<Empty>(neighbourBall->second))
-        direction.y = direction.y * -1;
-      else if (std::holds_alternative<Block>(aboveBall->second))
-        direction.y = direction.y * -1;
-
-      if (std::holds_alternative<Empty>(aboveBall->second))
-        direction.x = direction_.x * -1;
-      else if (std::holds_alternative<Block>(neighbourBall->second))
-        direction.x = direction_.x * -1;
-     }
-    else if (std::holds_alternative<Block>(neighbourBall->second))
-    {      
-      auto aboveBall = screen_.find({newBallPosition.x, newBallPosition.y + direction.y});
-      auto nextHitBall = screen_.find({newBallPosition.x - direction.x, newBallPosition.y + direction.y});
-      // #
-      //#@  
-      if (std::holds_alternative<Block>(aboveBall->second))
-        direction.x = direction_.x * -1;
-      //  #
-      //#@  
-      else if (std::holds_alternative<Block>(nextHitBall->second))
-      { // direction is kept except
-        // #
-        // #@
-        //   #
-        auto oppositeHit = screen_.find({newBallPosition.x + direction.x, newBallPosition.y - direction.y});
-        if (std::holds_alternative<Block>(oppositeHit->second))
-          direction.x = direction_.x * -1;
-      }
-      //#
-      //#@
+      break;
+    if (code == Intcode::Input)
+      moveJoystick(computer_, state);
+    if (code == Intcode::Output) {
+      auto output = computer_.readOutput();
+      if (output == -1)
+        updateScore();
       else
-        direction.x = direction_.x * -1;
-    }
-
-    return direction;
-  }
-
-  void ArcadeCabinet::drawTile(int x) {
-    auto y = getNextOutput();
-    auto tileId = getTileId(getNextOutput());
-    screen_[{x, y}] = tileId;
-    if (std::holds_alternative<Paddle>(tileId))
-      paddlePosition_ = {x,y};
-    if (std::holds_alternative<Ball>(tileId)) {
-      direction_ = predictDirection({x,y});
-      ballPosition_ = {x,y};
+        drawTile(computer_, screen, state, output);
     }
   }
+}
+
+int ArcadeCabinet::getScore() const { return score_; }
 
 
-  void ArcadeCabinet::printScreen() const {
-    auto [width, height] = getScreenDimension();
-    for (int y = 0; y < height; ++y) {
-      for (int x = 0; x < width; ++x) {
-        auto tile = screen_.find({x, y});
-        if (tile != std::end(screen_)) {
-          if (std::holds_alternative<Empty>(tile->second))
-            std::cout << ' ';
-          if (std::holds_alternative<Wall>(tile->second))
-            std::cout << 'W';
-          if (std::holds_alternative<Block>(tile->second))
-            std::cout << '#';
-          if (std::holds_alternative<Paddle>(tile->second))
-            std::cout << '-';
-          if (std::holds_alternative<Ball>(tile->second))
-            std::cout << '@';
-        } else
-          std::cout << ' ';
-      }
-      std::cout << '\n';
-    }
-  }
+void ArcadeCabinet::updateScore() {
+  auto y = getNextOutput(computer_);
+  if (y != 0)
+    throw std::runtime_error("should be zero for segment");
+
+  score_ = getNextOutput(computer_);
+}
