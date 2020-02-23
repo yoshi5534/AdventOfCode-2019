45adab646e49f9dc5dafb24d17ea6e50ec89bcca
yoshi <hess.jonathan@gmail.com>
2020-02-23 22:40:08 +0000

    added some more tests

--- spaceship/src/vault.cpp
+++ spaceship/src/vault.cpp
@@ -1,6 +1,7 @@
 #include <vault.h>
 
 #include <algorithm>
+#include <iostream>
 #include <set>
 #include <sstream>
 #include <vector>
@@ -33,6 +34,7 @@ Vault::Vault(std::istream &map) : vault_{} {
 }
 
 namespace {
+int const MAX_PATH = 100000;
 
 struct Node {
   uint g_, h_;
@@ -71,27 +73,52 @@ MapPosition findEntrance(VaultMap const &map) {
 
 bool isReachable(VaultMap const &map, MapPosition const &field) {
   return (std::holds_alternative<Open>(map.at(field)) ||
-          std::holds_alternative<Key>(map.at(field)) ||
-          std::holds_alternative<Entrance>(map.at(field)));
+          std::holds_alternative<Key>(map.at(field)));
 }
 
 bool isKey(VaultMap const &map, MapPosition const &field) {
   return std::holds_alternative<Key>(map.at(field));
 }
 
-bool hasDoors(VaultMap const &map) {
+bool isEntrance(VaultMap const &map, MapPosition const &field) {
+  return std::holds_alternative<Entrance>(map.at(field));
+}
+
+bool hasKeys(VaultMap const &map) {
   return std::find_if(std::begin(map), std::end(map), [](auto const &field) {
-           return std::holds_alternative<Door>(field.second);
+           return std::holds_alternative<Key>(field.second);
          }) != std::end(map);
 }
 
+void printMap(VaultMap const &map) {
+  int const width = 30;
+  int const height = 10;
+
+  for (int y = 0; y < height; ++y) {
+    for (int x = 0; x < width; ++x) {
+      if (map.find({x, y}) != std::end(map)) {
+        if (isKey(map, {x, y}))
+          std::cout << 'k';
+        else if (isEntrance(map, {x, y}))
+          std::cout << '@';
+        else if (isReachable(map, {x, y}))
+          std::cout << '.';
+        else
+          std::cout << '#';
+      }
+    }
+    std::cout << "\n";
+  }
+}
+
 int collectKey(VaultMap const &map, MapPosition const &start,
-                      int steps) {
+               MapPosition const &dir, int steps) {
 
   std::vector<MapPosition> directions = {{0, 1}, {1, 0}, {0, -1}, {-1, 0}};
   Node *current = nullptr;
   NodeSet openSet, closedSet;
-  openSet.insert(new Node(start));
+  openSet.insert(new Node(start + dir));
+  MapPosition keyPosition{-1, -1};
   while (!openSet.empty()) {
     current = *openSet.begin();
     for (auto node : openSet) {
@@ -100,8 +127,13 @@ int collectKey(VaultMap const &map, MapPosition const &start,
       }
     }
 
-    if (isKey(map, current->coordinates_))
+    if (isKey(map, current->coordinates_)) {
+      keyPosition = current->coordinates_;
+      printMap(map);
       break;
+    } else if (current->getScore() > MAX_PATH) {
+      break;
+    }
 
     closedSet.insert(current);
     openSet.erase(std::find(openSet.begin(), openSet.end(), current));
@@ -127,7 +159,9 @@ int collectKey(VaultMap const &map, MapPosition const &start,
     }
   }
 
-  auto keyPosition = current->coordinates_;
+  if (keyPosition.x < 0)
+    return MAX_PATH;
+
   char key = std::get<Key>(map.at(keyPosition)).key;
 
   int pathLength = 0;
@@ -136,35 +170,32 @@ int collectKey(VaultMap const &map, MapPosition const &start,
     current = current->parent_;
   }
 
-  if (!hasDoors(map))
-    return steps + pathLength;
-
-
-  auto doorPosition =
-      std::find_if(std::begin(map), std::end(map),
-                   [key](auto const &field) {
-                     return std::holds_alternative<Door>(field.second) &&
-                            std::get<Door>(field.second).door ==
-                                std::toupper(key);
-                   })
-          ->first;
-
+  auto door =
+      std::find_if(std::begin(map), std::end(map), [key](auto const &field) {
+        return std::holds_alternative<Door>(field.second) &&
+               std::get<Door>(field.second).door == std::toupper(key);
+      });
 
   releaseNodes(openSet);
   releaseNodes(closedSet);
 
   VaultMap newMap = map;
-  newMap[keyPosition] = Open{};
-  newMap[doorPosition] = Open{};
+  newMap[start] = Open{};
+  newMap[keyPosition] = Entrance{};
+  if (door != std::end(map))
+    newMap[door->first] = Open{};
 
-  int shortest = INT32_MAX;
+  if (!hasKeys(newMap))
+    return steps + pathLength;
+
+  int shortest = MAX_PATH;
   for (uint i = 0; i < directions.size(); ++i) {
-    MapPosition newCoordinates(keyPosition + directions[i]);
-    if (!isReachable(map, newCoordinates)) {
+    if (!isReachable(map, keyPosition + directions[i])) {
       continue;
     }
 
-    auto length = collectKey(newMap, newCoordinates, steps + pathLength + 1);
+    auto length =
+        collectKey(newMap, keyPosition, directions[i], steps + pathLength + 1);
     if (length < shortest)
       shortest = length;
   }
@@ -177,7 +208,16 @@ int collectKey(VaultMap const &map, MapPosition const &start,
 int Vault::collectKeys() {
   auto entrance = findEntrance(vault_);
   VaultMap map = vault_;
-  map[entrance] = Open{};
 
-  return collectKey(map, entrance, 0);
+  std::vector<MapPosition> directions = {{-1, 0}, {1, 0}, {0, 1}, {0, -1}};
+  std::vector<int> steps;
+
+  for (uint i = 0; i < directions.size(); ++i) {
+    if (!isReachable(map, entrance + directions[i])) {
+      continue;
+    }
+    steps.push_back(collectKey(map, entrance, directions[i], 1));
+  }
+
+  return *std::min_element (std::begin(steps), std::end(steps));
 }
--- unit-tests/vault.cpp
+++ unit-tests/vault.cpp
@@ -4,15 +4,76 @@
 #include <vault.h>
 
 using namespace AdventOfCode;
-
-TEST_CASE("Collect all keys") {
+/*
+TEST_CASE("Collect all keys in small example") {
   std::stringstream map;
   map << "#########\n"
          "#b.A.@.a#\n"
          "#########\n";
 
   Vault vault{map};
-  auto steps = vault.collectKeys ();
+  auto steps = vault.collectKeys();
   int expected = 8;
+  REQUIRE(expected == steps);
+}
+
+TEST_CASE("Collect all keys in larger example") {
+  std::stringstream map;
+  map << "########################\n"
+         "#f.D.E.e.C.b.A.@.a.B.c.#\n"
+         "######################.#\n"
+         "#d.....................#\n"
+         "########################\n";
+
+  Vault vault{map};
+  auto steps = vault.collectKeys();
+  int expected = 86;
+  REQUIRE(expected == steps);
+}
+
+TEST_CASE("Collect all keys in example 3") {
+  std::stringstream map;
+  map << "########################\n"
+         "#...............b.C.D.f#\n"
+         "#.######################\n"
+         "#.....@.a.B.c.d.A.e.F.g#\n"
+         "########################\n";
+
+  Vault vault{map};
+  auto steps = vault.collectKeys();
+  int expected = 132;
+  REQUIRE(expected == steps);
+}
+
+TEST_CASE("Collect all keys in example 4") {
+  std::stringstream map;
+  map << "#################\n"
+         "#i.G..c...e..H.p#\n"
+         "########.########\n"
+         "#j.A..b...f..D.o#\n"
+         "########@########\n"
+         "#k.E..a...g..B.n#\n"
+         "########.########\n"
+         "#l.F..d...h..C.m#\n"
+         "#################\n";
+
+  Vault vault{map};
+  auto steps = vault.collectKeys();
+  int expected = 136;
+  REQUIRE(expected == steps);
+}
+*/
+TEST_CASE("Collect all keys in example 4") {
+  std::stringstream map;
+  map << "########################\n"
+         "#@..............ac.GI.b#\n"
+         "###d#e#f################\n"
+         "###A#B#C################\n"
+         "###g#h#i################\n"
+         "########################\n";
+
+  Vault vault{map};
+  auto steps = vault.collectKeys();
+  int expected = 81;
   REQUIRE(expected == steps);
 }
