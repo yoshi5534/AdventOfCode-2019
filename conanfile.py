ecb9970d2e6af5fd298400466202671c0808b0c9
yoshi5534 <yoshi5534@users.noreply.github.com>
2021-02-14 20:03:13 +0000

    update dependencies

--- conanfile.py
+++ conanfile.py
@@ -2,7 +2,7 @@ from conans import ConanFile, CMake
 
 class AdventOfCode2019(ConanFile):
   settings = "compiler", "build_type"
-  requires = "doctest/2.3.4@bincrafters/stable", "cxxopts/2.0.0@conan-cpp/latest", "range-v3/0.10.0@ericniebler/stable"
+  requires = "doctest/2.3.4@bincrafters/stable", "cxxopts/2.2.1", "range-v3/0.10.0@ericniebler/stable"
   generators = "cmake"
 
   def build(self):
